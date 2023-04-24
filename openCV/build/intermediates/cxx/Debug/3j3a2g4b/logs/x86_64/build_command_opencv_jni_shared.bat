99432145ae083401d254430f15a1d58d0134422b
Niuan Lucas <niuanlucasnicolau@gmail.com>
2023-04-24 20:24:46 -0300

    OpenCV Image to Grayscale

---
+++ openCV/build/intermediates/cxx/Debug/3j3a2g4b/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\niuanlucas\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\AndroidStudio\\OpenCV4\\openCV\\.cxx\\Debug\\3j3a2g4b\\x86_64" ^
+  opencv_jni_shared
---
+++ openCV/build/intermediates/cxx/RelWithDebInfo/91v3i2r5/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\niuanlucas\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HD:\\AndroidStudio\\OpenCV4\\openCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\niuanlucas\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\niuanlucas\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\niuanlucas\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\niuanlucas\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=D:\\AndroidStudio\\OpenCV4\\openCV\\build\\intermediates\\cxx\\RelWithDebInfo\\91v3i2r5\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=D:\\AndroidStudio\\OpenCV4\\openCV\\build\\intermediates\\cxx\\RelWithDebInfo\\91v3i2r5\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BD:\\AndroidStudio\\OpenCV4\\openCV\\.cxx\\RelWithDebInfo\\91v3i2r5\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
